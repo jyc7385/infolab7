5382f4562208cf2f6111680420e480566200a72a
jyc7385 <jyc7385@naver.com>
2019-08-23 22:18:06 +0900

    my 4th commit

--- R/dominant.R
+++ R/dominant.R
@@ -13,7 +13,7 @@ dominant <- function(geno, n, samp, p, pi)
   geno_dom<-apply(geno_dom,2,function(x) {ifelse(x==-1, x <- 0, x <- 1)})
   pheno_dom<- sqrt(1-pi)*rnorm(n=300, mean=0, sd=1)+geno_dom[1,]*sqrt(pi/(p*(1-p)))
 
-  write.table(pheno, file="sample_phenotype_dom.txt", row.names=F, col.names=F, quote=F,sep="\t")
+  write.table(pheno_dom, file="sample_phenotype_dom.txt", row.names=F, col.names=F, quote=F,sep="\t")
 
-	return (pheno=pheno)
+	return (pheno_dom=pheno_dom)
 }
